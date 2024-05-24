a04b64800234f837f5c326caccb6a70d16e82252
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-05-24 13:38:11 +0300

    add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,13 @@ import { UpdateCategoryComponent } from './components/category/update-category/u
 import { PipesComponent } from './pages/pipes/pipes.component';
 import { TranslateExampleComponent } from './pages/translate-example/translate-example.component';
 import { DirectivesComponent } from './pages/directives/directives.component';
+import { LoginComponent } from './pages/_auth/login/login.component';
+import { SignupComponent } from './pages/_auth/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'signup', component: SignupComponent, title: 'Signup' },
   { path: 'products', component: ProductsComponents, title: 'Products' },
 
   { path: 'pipes', component: PipesComponent, title: 'Pipes' },
